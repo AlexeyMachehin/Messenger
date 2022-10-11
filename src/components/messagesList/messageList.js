82207e74fc1dd822520bcb5fa03c2cdfb4404f6f
Alexey <Lehamachehin@yandex.ru>
2022-10-12 03:02:55 +0500

    create select, shatPageInput components, fix styles

--- src/components/messagesList/messageList.js
+++ src/components/messagesList/messageList.js
@@ -1,16 +1,44 @@
-const addDeleteUserList = document.querySelector(".add-delete-user-list");
+const selectListHeader = document.querySelector(".select-list-header");
+const selectListFooter = document.querySelector(".select-list-footer");
+
+// const addFileList = document.querySelector(".add-file-list");
 
 const addDeleteUserButton = document.querySelector(".add-delete-user-button");
+const addFileButton =  document.querySelector(".messages-list__footer__add-file");
 
 const body = document.querySelector("body");
 
 addDeleteUserButton.addEventListener("click", (e) => {
+  selectListFooter.classList.remove("select-list-active");
   addDeleteUserButton.classList.toggle("add-delete-user-button-active");
-  addDeleteUserList.classList.toggle("add-delete-user-list-active");
+  selectListHeader.classList.toggle("select-list-active");
+  e.stopPropagation();
+});
+
+addFileButton.addEventListener("click", (e) => {
+  selectListHeader.classList.remove("select-list-active");
+  addFileButton.classList.toggle("add-delete-user-button-active");
+  addFileButton.classList.toggle("messages-list__footer__add-file-active");
+  selectListFooter.classList.toggle("select-list-active");
   e.stopPropagation();
 });
 
 body.addEventListener("click", () => {
   addDeleteUserButton.classList.remove("add-delete-user-button-active");
-  addDeleteUserList.classList.remove("add-delete-user-list-active");
+  addFileButton.classList.remove("messages-list__footer__add-file-active");
+  selectListHeader.classList.remove("select-list-active");
+  selectListFooter.classList.remove("select-list-active");
 });
+
+
+// addFileButton.addEventListener("click", (e) => {
+//   // addFileButton.classList.toggle("add-delete-user-button-active");
+//   addFileList.toggle("add-delete-user-list-active");
+//   e.stopPropagation();
+// });
+
+
+// body.addEventListener("click", () => {
+//   // addFileButton.classList.remove("add-delete-user-button-active");
+//   addFileList.remove("add-delete-user-list-active");
+// });
