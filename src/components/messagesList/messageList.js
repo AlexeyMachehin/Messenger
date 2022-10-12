ca7924d788d8fe812f6d866ae8fe9c66025eea45
Alexey <Lehamachehin@yandex.ru>
2022-10-12 19:17:01 +0500

    create errorSpanComponent, manageUserModal, deleteChatModal. fix styles

--- src/components/messagesList/messageList.js
+++ src/components/messagesList/messageList.js
@@ -1,44 +1,28 @@
-const selectListHeader = document.querySelector(".select-list-header");
-const selectListFooter = document.querySelector(".select-list-footer");
-
-// const addFileList = document.querySelector(".add-file-list");
-
+const selectListHeader = document.querySelector(
+  ".select-list-header-container"
+);
+const selectListFooter = document.querySelector(
+  ".select-list-footer-container"
+);
 const addDeleteUserButton = document.querySelector(".add-delete-user-button");
-const addFileButton =  document.querySelector(".messages-list__footer__add-file");
-
+const addFileButton = document.querySelector(
+  ".messages-list__footer__add-file"
+);
 const body = document.querySelector("body");
 
 addDeleteUserButton.addEventListener("click", (e) => {
-  selectListFooter.classList.remove("select-list-active");
-  addDeleteUserButton.classList.toggle("add-delete-user-button-active");
-  selectListHeader.classList.toggle("select-list-active");
+  selectListFooter.classList.remove("active");
+  selectListHeader.classList.toggle("active");
   e.stopPropagation();
 });
 
 addFileButton.addEventListener("click", (e) => {
-  selectListHeader.classList.remove("select-list-active");
-  addFileButton.classList.toggle("add-delete-user-button-active");
-  addFileButton.classList.toggle("messages-list__footer__add-file-active");
-  selectListFooter.classList.toggle("select-list-active");
+  selectListHeader.classList.remove("active");
+  selectListFooter.classList.toggle("active");
   e.stopPropagation();
 });
 
 body.addEventListener("click", () => {
-  addDeleteUserButton.classList.remove("add-delete-user-button-active");
-  addFileButton.classList.remove("messages-list__footer__add-file-active");
-  selectListHeader.classList.remove("select-list-active");
-  selectListFooter.classList.remove("select-list-active");
+  selectListHeader.classList.remove("active");
+  selectListFooter.classList.remove("active");
 });
-
-
-// addFileButton.addEventListener("click", (e) => {
-//   // addFileButton.classList.toggle("add-delete-user-button-active");
-//   addFileList.toggle("add-delete-user-list-active");
-//   e.stopPropagation();
-// });
-
-
-// body.addEventListener("click", () => {
-//   // addFileButton.classList.remove("add-delete-user-button-active");
-//   addFileList.remove("add-delete-user-list-active");
-// });
--- src/pages/changeData/changeData.js
+++ src/pages/changeData/changeData.js
@@ -1,10 +1,6 @@
-
 const openBtn = document.querySelector("#open");
-const closeBtn = document.querySelector(".close-modal-button")
+const closeBtn = document.querySelector(".close-modal-button");
 
 const dialog = document.querySelector("dialog");
-openBtn.addEventListener('click', () => dialog.showModal())
-closeBtn.addEventListener('click', () => dialog.close())
-
-// document.querySelector('.card').addEventListener('click', (e) => e.stopPropagation());
-// document.querySelector('dialog').addEventListener('click', (e) => e.target.close());
+openBtn.addEventListener("click", () => dialog.showModal());
+closeBtn.addEventListener("click", () => dialog.close());
