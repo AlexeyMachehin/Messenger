567b6d9f6212bfec2deb68865dd4e1fb93fe3c53
Alexey <Lehamachehin@yandex.ru>
2022-10-30 21:38:30 +0500

    create changePassword component

---
+++ src/pages/changePassword/changePassword.ts
@@ -0,0 +1,49 @@
+import GeneralButton from "../../components/generalButton/generalButton";
+import GeneralInput from "../../components/generalInput/generalInput";
+import Block from "../../utils/block";
+import { Props } from "../../utils/models/props";
+import { render } from "../../utils/renderDOM";
+import { changePasswordTemplate } from "./changePasswordTemplate";
+
+export default class ChangePassword extends Block {
+  constructor(
+    props: Props & {
+      avatarURL: string;
+      generalInputOldPassword: Block;
+      generalInputNewPassword: Block;
+      generalInputRepeatPassword: Block;
+      generalButtonSave: Block;
+    }
+  ) {
+    super("div", props);
+  }
+
+  render(): DocumentFragment {
+    return this.compile(changePasswordTemplate, this.props);
+  }
+}
+const changePassword: ChangePassword = new ChangePassword({
+  avatarURL:
+    "https://avatars.mds.yandex.net/i?id=90a14aacfb5159c04fc902bad5bbd095-5232129-images-thumbs&n=13&exp=1",
+  generalInputOldPassword: new GeneralInput({
+    label: "Old password",
+    type: "password",
+    name: "password",
+  }),
+  generalInputNewPassword: new GeneralInput({
+    label: "New password",
+    type: "password",
+    name: "password",
+  }),
+  generalInputRepeatPassword: new GeneralInput({
+    label: "Repeat password",
+    type: "password",
+    name: "password",
+    errorText: "Passwords don't match",
+  }),
+  generalButtonSave: new GeneralButton({
+    buttonText: "Save",
+  }),
+});
+
+render(".main", changePassword);
---
+++ src/pages/changePassword/changePasswordTemplate.ts
@@ -0,0 +1,5 @@
+export const changePasswordTemplate = `
+div !{goBackAside}
+    .card
+        .container #[h1 Change password] #[img(class="avatar" src=avatarURL alt="avatar")] !{generalInputOldPassword} !{generalInputNewPassword} !{generalInputRepeatPassword} !{generalButtonSave}
+`
